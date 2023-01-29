8262d62b83bc91002dfe93358e6d9561567c675d
root <root@ip-172-31-10-211.us-east-2.compute.internal>
2023-01-29 03:17:10 +0000

    finish cloud code

--- cloud/processor/processor.py
+++ cloud/processor/processor.py
@@ -1,7 +1,10 @@
 import logging
-import cv2
-import paho.mqtt.client as mqtt
+import os
 import boto3
+import paho.mqtt.client as mqtt
+import numpy as np
+from datetime import datetime
+
 
 # Set up logger.
 logging.basicConfig(level=logging.INFO)
@@ -12,13 +15,22 @@ LOCAL_MQTT_HOST="broker-cloud"
 LOCAL_MQTT_PORT=1883
 LOCAL_MQTT_TOPIC="faces-cloud"
 
+# Setup S3 config.
+s3 = boto3.resource('s3')
+
 # Define behavior on connect.
 def on_connect_local(client, userdata, flags, rc):
     logger.info(f"Connected to local broker with RC: {str(rc)}")
     client.subscribe(LOCAL_MQTT_TOPIC)
 # Define behavior on received msg. 
 def on_message_local(client, userdata, msg):
-    logger.info(f"{msg}")
+    # Reconstruct image.
+    image_string = np.frombuffer(msg.payload, np.byte).tostring()
+    # Put to S3 bucket.
+    s3.Bucket(os.environ["S3_BUCKET_NAME"]).put_object(
+            Key=str(datetime.now()) + ".png",  # format: '/my_image.png'
+            Body=image_string
+)
 
 # Setup connection to MQTT broker. 
 local_mqttclient = mqtt.Client()
